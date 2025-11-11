0f837a7a3c4f0ee843bbd262235d7ce03675b26d
Aliii Yasser <35704350+Aliii3@users.noreply.github.com>
2025-11-11 09:44:17 +0200

    Instructor.h

--- Instructor.h
+++ Instructor.h
@@ -1,19 +1,21 @@
 #ifndef INSTRUCTOR_H
 #define INSTRUCTOR_H
-
+#include <iostream>
+#include <string>
 #include "Person.h"
 using namespace std;
-//#write Instructor class here
+
+class Instructor  {
+public:
+    string department;
+    int experienceYears;
+
+void displayteacherInfo() {
+    cout << "Department: " << department << endl;
+    cout << "Years of Experience: " << experienceYears << endl;
+};
 
 
-
-
-
-
-
-
-
-
-
+};
 
 #endif
