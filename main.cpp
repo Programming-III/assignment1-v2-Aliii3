7d15206f564faf0bdb7ae1afd8add82976957b8e
Aliii Yasser <35704350+Aliii3@users.noreply.github.com>
2025-11-11 09:45:19 +0200

    Main.cpp

--- main.cpp
+++ main.cpp
@@ -9,40 +9,44 @@
 using namespace std;
 
 // ==================== Person Class Implementation =========================
-
-
-
-
-
-
-
+Person::Person() : name(""), id(0) {}
 
 // ==================== Student Class Implementation ====================
-
-
-
-
-
+Student::Student() : yearlevel(0), major("") {}
 
 // ==================== Instructor Class Implementation ====================
-
-
-
-
-
+Instructor::Instructor() : department(""), experienceYears(0) {}
 
 // ==================== Course Class Implementation ====================
-
-
-
-
-
-
-
+Course::Course()
+    : courseCode(""), courseTitle(""), maxStudents(0), students(nullptr), currentStudents(0) {}
 
 // ==================== Main Function ====================
 int main() {
+   Course course;
+void setCourseCode(string code) {
+    courseCode = code;
+}
+void setMaxStudents(int max) {
+    maxStudents = max;
+
+   course.courseTitle = "Introduction to Programming";
+   course.maxStudents = 3;
+   course.currentStudents = 1;
    
+   Instructor instructor;
+   instructor.name = "Dr. Lina Khaled";
+   instructor.experienceYears = 5;
+   instructor.department = "Computer Science";
+   
+   Student student;
+   student.name = "Omar Nabil";
+   student.yearlevel = 2;
+   student.major = "Informatics";
     
     return 0;
 }
+void setCourseTitle(string title) {
+    courseTitle = title;
+}
+};
