1d0363b7d3e174dafa856999e94754e3957ccca2
Aliii Yasser <35704350+Aliii3@users.noreply.github.com>
2025-11-11 09:43:47 +0200

    Course.h

--- Course.h
+++ Course.h
@@ -1,22 +1,36 @@
 #ifndef COURSE_H
 #define COURSE_H
-
 #include <string>
+#include <iostream>
 #include "Student.h"
 using namespace std;
-//#write your code here
 
+class Course {
+private:
 
+    string courseCode;
+    string courseTitle;
+    int maxStudents;
+    Student* students; 
+    int currentStudents;
+public:
+    Course()
+        : courseCode(), courseTitle(), maxStudents(0), students(nullptr), currentStudents(0) {}
 
+    void displayCourseInfo() const {
+        cout << "Course Code: " << courseCode << endl;
+        cout << "Course Title: " << courseTitle << endl;
+        cout << "Max Students: " << maxStudents << endl;
+        cout << "Current Enrolled Students: " << currentStudents << endl;
+    }
 
-
-
-
-
-
-
-
-
-
+    void addStudent(const Student& student) {
+        if (currentStudents < maxStudents) {
+            students[currentStudents++] = student;
+        } else {
+            cout << "Cannot add more students, course is full." << endl;
+        }
+    }
+};
 
 #endif
