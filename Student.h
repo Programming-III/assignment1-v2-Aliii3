eb0f8d476c73ffb1337247551e3e7e3528943f04
Aliii Yasser <35704350+Aliii3@users.noreply.github.com>
2025-11-11 09:44:59 +0200

    Student.h
    Added displaystudentInfo method to output student details.

--- Student.h
+++ Student.h
@@ -1,21 +1,20 @@
 #ifndef STUDENT_H
 #define STUDENT_H
-
 #include "Person.h"
+#include <iostream>
 #include <string>
-#using namespace std;
-//#write student class here
+using namespace std;
+class Student { 
+public:
 
+int yearlevel;
+string major;
 
+void displaystudentInfo() {
+    cout << "Year Level: " << yearlevel << endl;
+    cout << "Major: " << major << endl;
+}
 
-
-
-
-
-
-
-
-
-
+};
 
 #endif
